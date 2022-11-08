ecfed487835fb17bee532efcfc535ec50b65d8ed
Ivan <75212472+HiPriv228@users.noreply.github.com>
2022-11-08 23:51:46 +0500

    Update mi_relay_for_hikka.py

--- mi_relay_for_hikka.py
+++ mi_relay_for_hikka.py
@@ -33,29 +33,27 @@ self.config = loader.ModuleConfig(
 loader.ConfigValue(
 'DEVICE_IP',
 None,
-"Ip of device"
-)
-loader.ConfigValue(
+"Ip of device";
 'DEVICE_TOKEN',
 None,
 "Token of device"
 )
-)
 
 
 
 
-async def relayon(self, call: InlineCall):
+
+async def relayon(self, message):
                 DEVICE_IP = self.config['DEVICE_IP']
                 DEVICE_TOKEN = self.config['DEVICE_TOKEN']
-                print(plug.send("set_properties", [{'did': 'MYDID', 'siid': 2, 'piid': 1, 'value':True}]))
-#               await utils.answer(message, 'relaytrue')
+                plug.send("set_properties", [{'did': 'MYDID', 'siid': 2, 'piid': 1, 'value':True}])
+                await utils.answer(message, 'relaytrue')
           
-async def relayoff(self, call: InlineCall):
+async def relayoff(self, message):
                 DEVICE_IP = self.config['DEVICE_IP']
                 DEVICE_TOKEN = self.config['DEVICE_TOKEN']
-                print(plug.send("set_properties", [{'did': 'MYDID', 'siid': 2, 'piid': 1, 'value':False}]))
-#               await utils.answer(message, 'relayfalse')
+                plug.send("set_properties", [{'did': 'MYDID', 'siid': 2, 'piid': 1, 'value':False}])
+                await utils.answer(message, 'relayfalse')
 
 async def client_ready(self, client, db):
 
